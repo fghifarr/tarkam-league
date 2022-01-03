a2f6657699bcba5d28b94084d7c659193b4afb45
F Ghifar R <fghifarr@gmail.com>
2022-01-03 10:50:49 +0700

    Project and runtime configuration

---
+++ src/main/java/com/fghifarr/tarkamleague/TarkamLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.fghifarr.tarkamleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkamLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkamLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fghifarr/tarkamleague/TarkamLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fghifarr.tarkamleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarkamLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
