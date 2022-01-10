a85c578cca1b07377b3665605412b3f8aa86e019
Fathullah Ghifar <fghifarr@gmail.com>
2022-01-10 21:40:07 +0700

    Update Role constants to uppercase

--- src/main/java/com/fghifarr/tarkamleague/configs/constants/RoleConstant.java
+++ src/main/java/com/fghifarr/tarkamleague/configs/constants/RoleConstant.java
@@ -1,8 +1,8 @@
 package com.fghifarr.tarkamleague.configs.constants;
 
 public class RoleConstant {
-    public static final String ADMINISTRATOR = "Administrator";
-    public static final String CREATOR = "Creator";
-    public static final String EDITOR = "Editor";
-    public static final String VIEWER = "Viewer";
+    public static final String ADMINISTRATOR = "ADMINISTRATOR";
+    public static final String CREATOR = "CREATOR";
+    public static final String EDITOR = "EDITOR";
+    public static final String VIEWER = "VIEWER";
 }
