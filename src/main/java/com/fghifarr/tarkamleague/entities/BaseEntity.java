1c349246a1bbd1cbb99a7db928670d52ba1c7bbf
Fathullah Ghifar <fghifarr@gmail.com>
2022-01-16 18:16:29 +0700

    Remove unused import and update Base Entity's serial version uid

--- src/main/java/com/fghifarr/tarkamleague/entities/BaseEntity.java
+++ src/main/java/com/fghifarr/tarkamleague/entities/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 public class BaseEntity {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     @Id
     @GeneratedValue
